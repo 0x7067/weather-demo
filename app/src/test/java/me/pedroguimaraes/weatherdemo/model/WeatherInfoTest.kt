31ff8b735b7b5f819edeccd4a14dec72f670fe4b
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-05 17:39:32 -0300

    Add WeatheInfo specs

--- app/src/test/java/me/pedroguimaraes/weatherdemo/model/WeatherInfoTest.kt
+++ app/src/test/java/me/pedroguimaraes/weatherdemo/model/WeatherInfoTest.kt
@@ -1,5 +1,35 @@
 package me.pedroguimaraes.weatherdemo.model
 
-import org.junit.Assert.*
+import me.pedroguimaraes.weatherdemo.R
+import kotlin.test.assertEquals
+import org.junit.Test as test
 
-class WeatherInfoTest
+class WeatherInfoTest {
+
+    private val weatherInfo = WeatherInfo(37.8267,-122.4233, "partly-cloudy-day",
+            "Partly Cloudy", 65.12, 1.3, 6.98)
+
+    @test fun formatTemperature() {
+        assertEquals("65°", weatherInfo.getTemperature())
+    }
+
+    @test fun formatRainProbability() {
+        assertEquals("1%", weatherInfo.getRainProbability())
+    }
+
+    @test fun formatWindSpeed() {
+        assertEquals("7 mph", weatherInfo.getWindSpeed())
+    }
+
+    @test fun getsCorrectIcon() {
+        assertEquals(R.mipmap.partly_cloudy_day, weatherInfo.getWeatherIcon())
+    }
+
+    @test fun getsDefaultIconIfNotAvailable() {
+        val weatherInfo = WeatherInfo(37.8267,-122.4233, "gibberish",
+                "Partly Cloudy", 65.12, 1.3, 6.98)
+
+        assertEquals(R.mipmap.none_available, weatherInfo.getWeatherIcon())
+    }
+}
+
