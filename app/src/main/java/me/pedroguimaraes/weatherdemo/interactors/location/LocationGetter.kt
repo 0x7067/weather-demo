5f30d41fd12510b28ef344647b0759a83845405b
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-05 16:35:39 -0300

    Use DI in LocationGetter

--- app/src/main/java/me/pedroguimaraes/weatherdemo/interactors/location/LocationGetter.kt
+++ app/src/main/java/me/pedroguimaraes/weatherdemo/interactors/location/LocationGetter.kt
@@ -1,26 +1,23 @@
 package me.pedroguimaraes.weatherdemo.interactors.location
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import me.pedroguimaraes.weatherdemo.WeatherApplication
-import java.util.*
+import me.pedroguimaraes.weatherdemo.injection.DependencyInjection
 
 
 class LocationGetter : LocationListener {
 
-    private val locationManager: LocationManager
-    private val context: Context = WeatherApplication.getContext()
-    private var location: Location? = null
+    private val dependencyInjection = DependencyInjection(WeatherApplication.getContext())
 
-    init {
-        this.locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    }
+    private val locationManager: LocationManager = dependencyInjection.locationManager()
+    private val packageManager: PackageManager = dependencyInjection.packageManager()
+
+    private var location: Location? = null
 
     @SuppressLint("MissingPermission")
     fun getLocation(): Location? {
@@ -36,18 +33,18 @@ class LocationGetter : LocationListener {
     }
 
     fun getCityName(lat: Double, lon: Double): String {
-        val geocoder = Geocoder(context, Locale.getDefault())
+        val geocoder = dependencyInjection.geocoder()
         val addresses = geocoder.getFromLocation(lat, lon, 1)
         return addresses[0].locality
     }
 
     private fun hasNetworkAndIsEnabled(): Boolean {
-        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_NETWORK)
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_NETWORK)
                 && locationManager.isProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER)
     }
 
     private fun hasGpsAndIsEnabled(): Boolean {
-        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS) &&
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS) &&
                 locationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)
     }
 
