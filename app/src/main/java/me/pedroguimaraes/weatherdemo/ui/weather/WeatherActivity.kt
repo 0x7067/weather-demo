b8b1a364149b36a5385f65d2f125e1bf2c545b9c
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-05 01:17:28 -0300

    Remove uneeded safe calls

--- app/src/main/java/me/pedroguimaraes/weatherdemo/ui/weather/WeatherActivity.kt
+++ app/src/main/java/me/pedroguimaraes/weatherdemo/ui/weather/WeatherActivity.kt
@@ -2,6 +2,7 @@ package me.pedroguimaraes.weatherdemo.ui.weather
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,21 +13,21 @@ import me.pedroguimaraes.weatherdemo.model.WeatherInfo
 
 class WeatherActivity : AppCompatActivity(), WeatherContract.WeatherView {
 
-    private var weatherPresenter: WeatherPresenter? = null
-    private var permissionEnforcer: PermissionEnforcer? = null
+    lateinit var weatherPresenter: WeatherPresenter
+    lateinit var permissionEnforcer: PermissionEnforcer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         permissionEnforcer = PermissionEnforcer(this)
-        permissionEnforcer!!.requestPermissions()
+        permissionEnforcer.requestPermissions()
 
         weatherPresenter = WeatherPresenter()
-        weatherPresenter!!.attachView(this)
+        weatherPresenter.attachView(this)
 
 
-        srl_weather!!.setOnRefreshListener({ this.getCurrentWeather() })
+        srl_weather.setOnRefreshListener({ this.getCurrentWeather() })
     }
 
     override fun onResume() {
@@ -35,17 +36,17 @@ class WeatherActivity : AppCompatActivity(), WeatherContract.WeatherView {
     }
 
     private fun getCurrentWeather() {
-        if (permissionEnforcer!!.allPermissionsGranted()) {
-            weatherPresenter!!.onResume()
+        if (permissionEnforcer.allPermissionsGranted()) {
+            weatherPresenter.onResume()
         }
     }
 
     override fun showProgress() {
-        srl_weather!!.isRefreshing = true
+        srl_weather.isRefreshing = true
     }
 
     override fun hideProgress() {
-        srl_weather!!.isRefreshing = false
+        srl_weather.isRefreshing = false
     }
 
     override fun showMessage(message: Int) {
@@ -54,27 +55,27 @@ class WeatherActivity : AppCompatActivity(), WeatherContract.WeatherView {
     }
 
     override fun setCurrentWeather(weatherInfo: WeatherInfo) {
-        tv_city_temperature!!.text = weatherInfo.getTemperature()
-        tv_summary!!.text = weatherInfo.summary
-        tv_weather_rain!!.text = weatherInfo.getRainProbability()
-        tv_weather_wind!!.text = weatherInfo.getWindSpeed()
-        tv_city_name!!.text = weatherInfo.getCityName()
-        img_weather_icon!!.setImageResource(weatherInfo.getWeatherIcon()!!)
+        tv_city_temperature.text = weatherInfo.getTemperature()
+        tv_summary.text = weatherInfo.summary
+        tv_weather_rain.text = weatherInfo.getRainProbability()
+        tv_weather_wind.text = weatherInfo.getWindSpeed()
+        tv_city_name.text = weatherInfo.getCityName()
+        img_weather_icon.setImageResource(weatherInfo.getWeatherIcon()!!)
     }
 
     override fun onDestroy() {
-        weatherPresenter!!.detachView()
+        weatherPresenter.detachView()
         super.onDestroy()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissionEnforcer!!.enforcePermissions()
+        permissionEnforcer.enforcePermissions()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         if (requestCode == PermissionEnforcer.APP_SETTINGS_REQUEST_CODE) {
-            permissionEnforcer!!.enforcePermissions()
+            permissionEnforcer.enforcePermissions()
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
