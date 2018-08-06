bcf0318465a346e1e7a2437ff432adbde2309946
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-06 18:25:25 -0300

    Create Config file with constants

---
+++ app/src/main/java/me/pedroguimaraes/weatherdemo/Config.java
@@ -0,0 +1,6 @@
+package me.pedroguimaraes.weatherdemo;
+
+public class Config {
+    public static final String BASE_URL = "https://api.darksky.net/forecast/ab3df38db111c4ac834b442f32cde995/";
+    public static final String UNITS = "si";
+}
