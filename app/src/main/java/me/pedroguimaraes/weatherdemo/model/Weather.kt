d8869db3221d9eb90fdb4a9a0d3e5156310019cd
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-09 20:03:59 -0300

    Rename Currently to a more descriptive name

--- app/src/main/java/me/pedroguimaraes/weatherdemo/model/Weather.kt
+++ app/src/main/java/me/pedroguimaraes/weatherdemo/model/Weather.kt
@@ -1,16 +1,16 @@
 package me.pedroguimaraes.weatherdemo.model
 
 data class Weather(val latitude: Double, val longitude: Double, val timezone: String,
-				   val offset: Int, val currently: Currently) {
+				   val offset: Int, val currentWeather: CurrentWeather) {
 
     fun getWeatherInfo(): WeatherInfo {
         val lat = this.latitude
         val lon = this.longitude
-        val icon = this.currently.icon
-        val summary = this.currently.summary
-        val temperature = this.currently.temperature
-        val rainProbability = this.currently.precipProbability
-        val windSpeed = this.currently.windSpeed
+        val icon = this.currentWeather.icon
+        val summary = this.currentWeather.summary
+        val temperature = this.currentWeather.temperature
+        val rainProbability = this.currentWeather.precipProbability
+        val windSpeed = this.currentWeather.windSpeed
 
         return WeatherInfo(lat, lon, icon, summary, temperature, rainProbability, windSpeed)
     }
