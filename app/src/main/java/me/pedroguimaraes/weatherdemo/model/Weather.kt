58cbc230a35881186082e965959366c92126b0c4
Pedro Guimarães <moisespedro15@gmail.com>
2018-08-10 07:40:36 -0300

    Revert "Rename Currently to a more descriptive name"
    This reverts commit d8869db3221d9eb90fdb4a9a0d3e5156310019cd.

--- app/src/main/java/me/pedroguimaraes/weatherdemo/model/Weather.kt
+++ app/src/main/java/me/pedroguimaraes/weatherdemo/model/Weather.kt
@@ -1,16 +1,16 @@
 package me.pedroguimaraes.weatherdemo.model
 
 data class Weather(val latitude: Double, val longitude: Double, val timezone: String,
-        val offset: Int, val currentWeather: CurrentWeather) {
+        val offset: Int, val currently: Currently) {
 
     fun getWeatherInfo(): WeatherInfo {
         val lat = this.latitude
         val lon = this.longitude
-        val icon = this.currentWeather.icon
-        val summary = this.currentWeather.summary
-        val temperature = this.currentWeather.temperature
-        val rainProbability = this.currentWeather.precipProbability
-        val windSpeed = this.currentWeather.windSpeed
+        val icon = this.currently.icon
+        val summary = this.currently.summary
+        val temperature = this.currently.temperature
+        val rainProbability = this.currently.precipProbability
+        val windSpeed = this.currently.windSpeed
 
         return WeatherInfo(lat, lon, icon, summary, temperature, rainProbability, windSpeed)
     }
